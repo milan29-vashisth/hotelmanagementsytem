408354ee298a473436ed9c43edde3c89e0854b1e
Milan <deepanshu.v7@gmail.com>
2026-05-28 14:25:38 +0530

    Initial hotel service app

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/rooms', (req, res) => {
+    res.render('rooms');
+});
+
+app.get('/booking', (req, res) => {
+    res.render('booking');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
